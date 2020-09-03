630565c77bb39f8b1b1ad87fc6c1ff3ba73f6d65
Ryoichi Tanaka <ryoichitanakajp@gmail.com>
2020-09-03 16:54:07 +0900

    Initial Commit

---
+++ SLTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SLTest
+//
+//  Created by 田中亮一 on 2020/09/03.
+//  Copyright © 2020 GOGA, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
